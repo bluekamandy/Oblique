0e052dfaa2a1791ce83d4bc6a8ae3fdb0d09e33c
Masood <masood@mac-pro.local>
2016-12-29 15:28:26 -0800

    Initial Commit

---
+++ Oblique/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oblique
+//
+//  Created by Masood on 12/29/16.
+//  Copyright © 2016 Masood Kamandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
